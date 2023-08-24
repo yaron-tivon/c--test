7adcc76996aaa7eb634fb8fac62f6d480cd7b6c8
yaron <yaron@tivon.ort.org.il>
2023-08-24 14:15:32 +0300

    double

--- Input/Program.cs
+++ Input/Program.cs
@@ -7,6 +7,8 @@
             int num1;
             num1 = int.Parse(Console.ReadLine());
             Console.WriteLine($"square of {num1} is {num1*num1}");
+            Console.WriteLine($"double {num1} is {num1*2}");
+            Console.ReadLine();
         }
     }
 }
