7d9e157f87c8a05b7b8a597db55a9e3413664b28
yaron <yaron@tivon.ort.org.il>
2023-08-24 14:11:28 +0300

    Input

---
+++ Input/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"square of {num1} is {num1*num1}");
+        }
+    }
+}
