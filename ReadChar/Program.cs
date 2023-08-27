bce536297806c02d04aa3d586f209c4a3efb3c35
yaron-tivon <143078723+yaron-tivon@users.noreply.github.com>
2023-08-27 13:22:58 +0300

    Add files via upload

---
+++ ReadChar/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ReadChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1;
+            Console.WriteLine("Enter a char:");
+            c1 = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{c1}{c1}{c1}");
+        }
+    }
+}
